990f5cd7801c2e0930ff06192eb6add0344a8bd4
Trevor Rapp <trrapp@gmail.com>
2022-07-20 09:53:08 -0600

    creates functionality to randomly display dice at the beginning of the game

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 (() => {
-  const k, 
-  const l, 
-  const m, 
-  const n, 
-  const o, 
-  const p
-  alert('js connected')
+  const objArray = [
+    {
+      'letter' : 'k',
+      'sidevalue' : 1
+    },
+    {
+      'letter' : 'l',
+      'sidevalue' : 2
+    },
+    {
+      'letter' : 'm',
+      'sidevalue' : 3
+    },
+    {
+      'letter' : 'n',
+      'sidevalue' : 4
+    },
+    {
+      'letter' : 'o',
+      'sidevalue' : 5
+    },
+    {
+      'letter' : 'p',
+      'sidevalue' : 6
+    },
+  ]
+
+  const diceOne = document.getElementById('dice-1');
+  const diceTwo = document.getElementById('dice-2')
+
+  function produceRandomNumber (maxvalue, startvalue) {
+    return Math.floor((Math.random() * maxvalue) + startvalue)
+  }
+
+  function setDisplayNumber (objArray) {
+    let a = produceRandomNumber(6, 1);
+    return objArray[a-1].letter
+  }
+
+  window.addEventListener('load', () => {
+    diceOne.textContent = setDisplayNumber(objArray);
+    diceTwo.textContent = setDisplayNumber(objArray);
+  })
+
 })();
