766767b4f83fdbb602ae9a270123e52e45e56219
Trevor Rapp <trrapp@gmail.com>
2022-07-21 20:48:57 -0600

    refactor for DRYness

--- index.js
+++ index.js
@@ -1,120 +1,141 @@
 (() => {
-
   // due to the type of font that is downloaded that will show dice as dot values instead of numbers, to get the right display for a dice we have to refer to it by the letter corresponding to the font.  Therefore it became useful to create an array of objects that associated the number value and letter that it corresponded to.
   const objArray = [
     {
-      'letter' : 'k',
-      'sidevalue' : 1
+      letter: "k",
+      sidevalue: 1,
     },
     {
-      'letter' : 'l',
-      'sidevalue' : 2
+      letter: "l",
+      sidevalue: 2,
     },
     {
-      'letter' : 'm',
-      'sidevalue' : 3
+      letter: "m",
+      sidevalue: 3,
     },
     {
-      'letter' : 'n',
-      'sidevalue' : 4
+      letter: "n",
+      sidevalue: 4,
     },
     {
-      'letter' : 'o',
-      'sidevalue' : 5
+      letter: "o",
+      sidevalue: 5,
     },
     {
-      'letter' : 'p',
-      'sidevalue' : 6
-    }
+      letter: "p",
+      sidevalue: 6,
+    },
   ];
 
-    // grab necessary elements that need to be populated
+  // grab necessary elements that need to be populated
 
-  const diceOne = document.getElementById('dice-1');
-  const diceTwo = document.getElementById('dice-2');
-  const scoreOneContainer = document.getElementById('score-1');
-  const scoreTwoContainer = document.getElementById('score-2');
-  const messageContainer = document.getElementById('message');
-  const button = document.getElementById('button');
+  const diceOne = document.getElementById("dice-1");
+  const diceTwo = document.getElementById("dice-2");
+  const scoreOneContainer = document.getElementById("score-1");
+  const scoreTwoContainer = document.getElementById("score-2");
+  const messageContainer = document.getElementById("message");
+  const button = document.getElementById("button");
 
   let player1score = 0;
   let player2score = 0;
-  let player1;
-  
+  let player1turn;
+
   // create functions
-  function produceRandomNumber (maxvalue, startvalue) {
-    return Math.floor((Math.random() * maxvalue) + startvalue);
+  function produceRandomNumber(maxvalue, startvalue) {
+    return Math.floor(Math.random() * maxvalue + startvalue);
   }
 
-  function setDisplayNumber (objArray, num) {
+  function setDisplayNumber(objArray, num) {
     // let a = produceRandomNumber(6, 1);
-    return objArray[num-1].letter;
+    return objArray[num - 1].letter;
   }
 
-  function playerRolls (player1, objArray) {
-    console.log(`entering playerRolls: player1 is ${player1} and objArray is ${objArray}`);
-    let randNum = produceRandomNumber(6, 1);
-    if (player1) {
+  function updateScore(player1turn, randNum) {
+    if (player1turn) {
       player1score += objArray[randNum - 1].sidevalue;
-      diceOne.textContent = setDisplayNumber(objArray, randNum);
-      scoreOneContainer.textContent = player1score;
-      console.log(`playerRolls if statement: player is true. randNum is ${randNum} and player1score is ${player1score}`);
-    } else if (!player1) {
+    } else {
       player2score += objArray[randNum - 1].sidevalue;
-      diceTwo.textContent = setDisplayNumber(objArray, randNum);
-      scoreTwoContainer.textContent = player2score;
-      console.log(`playerRolls else statement: player is false. randNum is ${randNum} and player1score is ${player1score}`);
     }
   }
-  
-  function changePlayer (player1) {
-    console.log(`entering changePlayer(); player1 is ${player1}`);
-    if (player1) {
-      
+
+  function updatePlayerMessage(player1turn, player1score, player2score) {
+    if (player1turn) {
+      scoreOneContainer.textContent = `${player1score}`;
+    } else {
+      scoreTwoContainer.textContent = `${player2score}`;
+    }
+  }
+
+  function updateDice(objArray, randNum, func) {
+    if (player1turn) {
+      diceOne.textContent = setDisplayNumber(objArray, randNum);
+    } else {
+      diceTwo.textContent = setDisplayNumber(objArray, randNum);
+    }
+  }
+
+  function playerRolls(player1turn, objArray) {
+    let randNum = produceRandomNumber(6, 1);
+
+    if (player1turn) {
+      updateScore(player1turn, randNum);
+      updateDice(objArray, randNum, setDisplayNumber);
+      updatePlayerMessage(player1turn, player1score, player2score);
+    } else if (!player1turn) {
+      updateScore(player1score, randNum);
+      updateDice(objArray, randNum, setDisplayNumber);
+      updatePlayerMessage(player1turn, player1score, player2score);
+    }
+  }
+
+  function changeMessage(player1turn) {
+    if (player1turn) {
       messageContainer.textContent = "Player 1's turn!";
-      diceTwo.classList.remove('active');
-      diceOne.classList.add('active');
-      player1 = false;
-      console.log(`changePlayer: true, player1 is ${player1}`);
     } else {
       messageContainer.textContent = "Player 2's turn";
-      diceTwo.classList.remove('active');
-      diceOne.classList.add('active');
-      player1 = true;
-      console.log(`changePlayer: false, player1 is ${player1}`);
     }
   }
 
-  function determineWhoRollsFirst () {
+  function changePlayer(player1turn) {
+    player1turn = !player1turn;
+  }
+
+  function determineWhoRollsFirst() {
     let a = produceRandomNumber(2, 0);
-    console.log("determineWHoRollsFirst: " + a);
-    if ( a === 0) {
-      player1 = true;
-      console.log("determineWhoRollsFirst: player1 = " + player1);
+    console.log(`DETERMINE WHO ROLLS FIRST (): random number is ${a}`);
+    if (a === 0) {
+      player1turn = true;
+      console.log(
+        `DETERMINE WHO ROLLS FIRST (), if statement: player1 is ${player1turn}`
+      );
     } else {
-      player1 = false;
-      console.log("determineWhoRollsFirst: player1 = " + player1);
+      player1turn = false;
+      console.log(
+        `DETERMINE WHO ROLLS FIRST (), else statement: player1 is ${player1turn}`
+      );
     }
   }
 
-  window.addEventListener('load', () => {
+  // add event listeners
+
+  window.addEventListener("load", () => {
     let a = produceRandomNumber(6, 1);
     let b = produceRandomNumber(6, 1);
     diceOne.textContent = setDisplayNumber(objArray, a);
     diceTwo.textContent = setDisplayNumber(objArray, b);
   });
 
-  button.addEventListener('click', (player1) => {
-    console.log(`CLICK EVENT: player 1 is ${player1}`)
-    if (player1 === undefined) {
-      console.log(`CLICK EVENT, IF STATEMENT: ${player1}`);
+  button.addEventListener("click", () => {
+    console.log(`CLICK EVENT: player 1 is ${player1turn}`);
+    if (player1turn === undefined) {
+      console.log(`CLICK EVENT, IF STATEMENT: ${player1turn}`);
       determineWhoRollsFirst();
       button.textContent = "Roll";
     } else {
-      console.log(`CLICK EVENT, ELSE STATEMENT: ${player1}`);
-      playerRolls(player1, objArray);
-      changePlayer(player1)
+      console.log(`CLICK EVENT, ELSE STATEMENT: ${player1turn}`);
+      playerRolls(player1turn, objArray);
+      changePlayer(player1turn);
+      changeMessage(player1turn);
     }
   });
-
 })();
