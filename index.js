d904265669bbe4fae6bf443abf72d3440ab5e1ab
Trevor Rapp <trrapp@gmail.com>
2022-07-29 01:26:04 -0600

    fixes not having correct event name in listener

--- index.js
+++ index.js
@@ -215,14 +215,14 @@
     }
   });
 
-  window.addEventListener(ondevicemotion, (evt) => {
+  window.addEventListener("devicemotion", (evt) => {
     alert("window ondevice motion");
     if (DeviceMotionEventRotationRate) {
       alert("determined motion is a rotation event");
       if (
-        evt.rotationRate.alpha > 1 ||
-        evt.rotationRate.beta > 1 ||
-        evt.rotationRate.gamma > 1
+        evt.rotationRate.alpha > 10 ||
+        evt.rotationRate.beta > 10 ||
+        evt.rotationRate.gamma > 10
       ) {
         alert("rotation exceeded 10");
         if (player1turn === undefined) {
