d18d0e0bac1c51d4308d86fae76a20684b26a8f6
Trevor Rapp <trrapp@gmail.com>
2022-07-30 23:49:14 -0600

    removes unnecessary files and makes title more prominent

--- susanna.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  let extRef = JSON.parse(localStorage.getItem("nuskin.account")).user.id;
-  if (typeof QSI === "undefined") {
-    QSI = {};
-    QSI.config = {
-      externalReference: extRef,
-    };
-  }
-})();
